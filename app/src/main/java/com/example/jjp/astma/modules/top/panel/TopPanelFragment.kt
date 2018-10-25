acbacc7021c80cb98001d7e31aa4e2460237b962
JuliaMirzabayeva <mirzabayevajulia@gmail.com>
2018-10-25 21:27:29 +0300

    replace month/year picker

--- app/src/main/java/com/example/jjp/astma/modules/top/panel/TopPanelFragment.kt
+++ app/src/main/java/com/example/jjp/astma/modules/top/panel/TopPanelFragment.kt
@@ -5,15 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.jjp.astma.R
-import com.rackspira.kristiawan.rackmonthpicker.RackMonthPicker
 import kotlinx.android.synthetic.main.fragment_top_panel.*
 import nucleus.factory.RequiresPresenter
 import nucleus.view.NucleusFragment
+import com.twinkle94.monthyearpicker.picker.YearMonthPickerDialog
+import java.text.SimpleDateFormat
 import java.util.*
+
 
 @RequiresPresenter(TopPanelFragmentPresenter::class)
 class TopPanelFragment : NucleusFragment<TopPanelFragmentPresenter>() {
-    private var picker: RackMonthPicker? = null
+    private var picker: YearMonthPickerDialog? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_top_panel, container, false)
@@ -25,17 +27,18 @@ class TopPanelFragment : NucleusFragment<TopPanelFragmentPresenter>() {
         chartDateLabel.setOnClickListener { _ -> openMonthYearPicker() }
     }
 
-    private fun openMonthYearPicker() {
-        picker = RackMonthPicker(activity)
-                .setLocale(Locale.getDefault())
-                .setPositiveButton { month, startDate, endDate, year, monthLabel ->
+    private fun setDefaultPickerDate(){
 
-                }
-                .setNegativeButton { _ -> closePicker() }
-        picker?.show()
     }
 
-    private fun closePicker() {
-        picker?.dismiss()
+    private fun openMonthYearPicker() {
+        picker = YearMonthPickerDialog(activity, YearMonthPickerDialog.OnDateSetListener { year, month ->
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            val simpleDateFormat = SimpleDateFormat("LLLL yyyy", Locale("ru"))
+            chartDateLabel.text = simpleDateFormat.format(calendar.time)
+        })
+        picker?.show()
     }
 }
