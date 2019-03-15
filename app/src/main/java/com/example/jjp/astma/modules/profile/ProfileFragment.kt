0a4414e9d48cfe8acde1a2ca81d268c2021ed747
JuliaMirzabayeva <mirzabayevajulia@gmail.com>
2019-03-15 12:49:47 +0300

    logout

--- app/src/main/java/com/example/jjp/astma/modules/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/jjp/astma/modules/profile/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package com.example.jjp.astma.modules.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.jjp.astma.R
+import com.example.jjp.astma.modules.login.LoginActivity
+import kotlinx.android.synthetic.main.fragment_profile.*
 import nucleus.factory.RequiresPresenter
 import nucleus.view.NucleusFragment
 
@@ -13,5 +16,18 @@ class ProfileFragment : NucleusFragment<ProfileFragmentPresenter>() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        logoutButton.setOnClickListener {
+            goToLoginActivity()
+        }
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(activity.baseContext, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
